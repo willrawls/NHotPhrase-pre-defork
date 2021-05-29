795e8b6b0099ec0317a43c71e1673aa2a09c5612
William M. Rawls <william.rawls@gmail.com>
2021-05-29 12:02:25 -0700

    Update TriggerTests.cs
    This proves sequence matching routine doesn't work right all the time for either expected true or false conditions. More work to do. Randomized keys added for additional shake down

--- src/NHotPhrase.WindowsForms.Tests/TriggerTests.cs
+++ src/NHotPhrase.WindowsForms.Tests/TriggerTests.cs
@@ -121,13 +121,62 @@ namespace NHotPhrase.WindowsForms.Tests
         }
 
         [DataTestMethod]
+        [DataRow(new[]{ Keys.A }, new[]{ Keys.A })]
+        public void VariousSequences_IsAMatch_True(Keys[] simulatedHistory, Keys[] sequence)
+        {
+            for (var i = 0; i < 100; i++)
+            {
+                var simulatedHistoryList = simulatedHistory.ToList();
+                var sequenceList = sequence.ToList();
+
+                VariousSequences_PokingAround(simulatedHistoryList, sequenceList, true);
+
+                simulatedHistoryList.Insert(0, RandomKey());
+                VariousSequences_PokingAround(simulatedHistoryList, sequenceList, true);
+            
+                simulatedHistoryList.Add(RandomKey());
+                VariousSequences_PokingAround(simulatedHistoryList, sequenceList, false);
+            }
+        }
+
+        [DataTestMethod]
         [DataRow(new[]{ Keys.A }, new[]{ Keys.B })]
         public void VariousSequences_IsAMatch_False(Keys[] simulatedHistory, Keys[] sequence)
         {
-            var hotPhraseKeySequence = new HotPhraseKeySequence("Fred", sequence, (sender, args) => args.Handled = true);
+            for (var i = 0; i < 100; i++)
+            {
+                var simulatedHistoryList = simulatedHistory.ToList();
+                var sequenceList = sequence.ToList();
+
+                VariousSequences_PokingAround(simulatedHistoryList, sequenceList, false);
+            
+                simulatedHistoryList.Insert(0, RandomKey());
+                VariousSequences_PokingAround(simulatedHistoryList, sequenceList, false);
+            
+                simulatedHistoryList.Add(RandomKey());
+                VariousSequences_PokingAround(simulatedHistoryList, sequenceList, false);
+            }
+        }
+
+        public static void VariousSequences_PokingAround(List<Keys> simulatedHistory, List<Keys> sequence, bool expected)
+        {
+            var hotPhraseKeySequence = new HotPhraseKeySequence("Fred", sequence.ToArray(), (sender, args) => args.Handled = true);
             var keyHistory = new KeyHistory(8, 8, DateTime.Now, simulatedHistory.ToList());
             var actual = hotPhraseKeySequence.IsAMatch(keyHistory);
-            Assert.IsFalse(actual);
+            var debugText = $"\n    Sequence: {KeyListToString(sequence)}\n     History: {KeyListToString(simulatedHistory)}";
+            Assert.AreEqual(expected, actual, debugText);
+        }
+
+        private static string KeyListToString(List<Keys> list)
+        {
+            return list.Aggregate("", (current, item) => 
+                current + (" " + item));
+        }
+
+        public static Random Random = new Random();
+        public static Keys RandomKey()
+        {
+            return (Keys) Random.Next(50, 150);
         }
     }
 }
